e0b7dd7304ccf65c66b22c2e23bc67f6441f1b96
Yash <yash.turmbekar@quasardigital.com>
2025-04-16 00:28:06 +0530

    bcrypt issue resolved

--- src/orders/auth.service.ts
+++ src/orders/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import * as bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 
 @Injectable()
 export class AuthService {
