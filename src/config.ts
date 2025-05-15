623c97d687cc36e984203f7c94bd7916f7dd05e2
Yash <yash.turmbekar@quasardigital.com>
2025-05-15 11:14:02 +0530

    added cors config for new domain

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@ export const corsConfig = {
     "https://mango.goprobaba.com",
     "http://mango.goprobaba.com.s3-website-us-east-1.amazonaws.com",
     "http://localhost:5173",
+    "http://mangoes.atyourdoorstep.shop.s3-website-us-east-1.amazonaws.com",
+    "https://mangoes.atyourdoorstep.shop",
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
